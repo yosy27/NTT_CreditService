ae0b2ef4e5ccee6ad8e3d147a55a155505cf563e
Lisbeth <yoselynespinoza27@gmail.com>
2025-08-14 00:41:06 -0500

    initial commit for CreditService microservice

---
+++ src/main/java/com/nttdata/credit_service/CreditServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.credit_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditServiceApplication.class, args);
+	}
+
+}
