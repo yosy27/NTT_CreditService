999f524d6d822c80de77b175f01fcf32dba0cdd2
Lisbeth <yoselynespinoza27@gmail.com>
2025-08-15 23:21:38 -0500

    update main application

--- src/main/java/com/nttdata/credit_service/CreditServiceApplication.java
+++ src/main/java/com/nttdata/credit_service/CreditServiceApplication.java
@@ -3,7 +3,8 @@ package com.nttdata.credit_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// * Clase principal del microservicio Credit Service.
+@SpringBootApplication(scanBasePackages = {"com.nttdata"})
 public class CreditServiceApplication {
 
 	public static void main(String[] args) {
