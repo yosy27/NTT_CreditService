7ca53b7ad917a031d4ada768e5ed6bf88102d83d
Lisbeth <yoselynespinoza27@gmail.com>
2025-08-15 23:13:28 -0500

    add reactive CreditRepository with custom queries

---
+++ src/main/java/com/nttdata/credit_service/persistence/CreditRepository.java
@@ -0,0 +1,15 @@
+package com.nttdata.credit_service.persistence;
+
+import com.nttdata.credit.model.CreditStatus;
+import com.nttdata.credit.model.CreditType;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Repositorio reactivo para la entidad CreditDocument.
+ */
+public interface CreditRepository extends ReactiveMongoRepository<CreditDocument, String> {
+    Flux<CreditDocument> findByCustomerId(String customerId);
+    Mono<Boolean> existsByCustomerIdAndTypeAndStatus(String customerId, CreditType type, CreditStatus status);
+}
