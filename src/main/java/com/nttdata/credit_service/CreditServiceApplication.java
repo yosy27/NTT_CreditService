6c6fa57542cbfd23b971aa56cc7a5d7ed6624c23
Lisbeth <yoselynespinoza27@gmail.com>
2025-09-02 08:23:03 -0500

    chore: adjust application base package scanning

--- src/main/java/com/nttdata/credit_service/CreditServiceApplication.java
+++ src/main/java/com/nttdata/credit_service/CreditServiceApplication.java
@@ -3,8 +3,10 @@ package com.nttdata.credit_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// * Clase principal del microservicio Credit Service.
-@SpringBootApplication(scanBasePackages = {"com.nttdata"})
+@SpringBootApplication(scanBasePackages = {
+		"com.nttdata.credit_service",
+		"com.nttdata.credit"
+})
 public class CreditServiceApplication {
 
 	public static void main(String[] args) {
