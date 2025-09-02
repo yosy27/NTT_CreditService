df9f8b03aeb20a5b4f4d969d8ece0ca298ea2a11
Lisbeth <yoselynespinoza27@gmail.com>
2025-09-02 08:20:08 -0500

    refactor(repository): update CreditRepository with annotations and string-based queries

--- src/main/java/com/nttdata/credit_service/repository/CreditRepository.java
+++ src/main/java/com/nttdata/credit_service/repository/CreditRepository.java
@@ -1,16 +1,18 @@
 package com.nttdata.credit_service.repository;
 
-import com.nttdata.credit.model.CreditStatus;
-import com.nttdata.credit.model.CreditType;
 import com.nttdata.credit_service.model.CreditDocument;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
  * Repositorio reactivo para la entidad CreditDocument.
  */
+@Repository
 public interface CreditRepository extends ReactiveMongoRepository<CreditDocument, String> {
+    Mono<CreditDocument> findById(String customerId);
     Flux<CreditDocument> findByCustomerId(String customerId);
-    Mono<Boolean> existsByCustomerIdAndTypeAndStatus(String customerId, CreditType type, CreditStatus status);
+    Mono<Boolean> existsByCustomerIdAndTypeAndStatus(String customerId, String type, String status);
+
 }
