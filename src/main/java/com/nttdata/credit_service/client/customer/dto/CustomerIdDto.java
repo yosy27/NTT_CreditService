57edacd9b92f413b778d30195d7e4ce20d93a30f
Lisbeth <yoselynespinoza27@gmail.com>
2025-09-04 17:09:27 -0500

    feat(dto): add CardDTO and CustomerIdDto for credit service

---
+++ src/main/java/com/nttdata/credit_service/client/customer/dto/CustomerIdDto.java
@@ -0,0 +1,14 @@
+package com.nttdata.credit_service.client.customer.dto;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CustomerIdDto {
+
+    @JsonAlias({"id", "customerId"})
+    private String id;
+    private String documentNumber;
+}
---
+++ src/main/java/com/nttdata/credit_service/dto/CardDTO.java
@@ -0,0 +1,25 @@
+package com.nttdata.credit_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Pattern;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CardDTO {
+
+    /** ID/token interno de la tarjeta */
+    private String id;
+
+    /** Últimos 4 dígitos */
+    @Pattern(regexp = "^[0-9]{4}$")
+    private String last4;
+
+    /** VISA | MASTERCARD | AMEX | OTRAS (libre). */
+    private String brand;
+}
